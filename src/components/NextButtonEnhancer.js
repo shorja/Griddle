a0f8839ecee55c6bcdbc54a30f6a62e844b5448d
Short, James <James_Short@intuit.com>
2017-09-12 16:42:32 -0600

    Revert "Move pagination redux actions to enhancer"
    This reverts commit e8ca03c1302881cc30186c4641ac325957b82dda.

--- src/components/NextButtonEnhancer.js
+++ src/components/NextButtonEnhancer.js
@@ -3,18 +3,15 @@ import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import mapProps from 'recompose/mapProps';
 import getContext from 'recompose/getContext';
-import { getNext } from '../actions';
 import { combineHandlers } from '../utils/compositionUtils';
-import { connect } from '../utils/griddleConnect';
 
 const enhance = OriginalComponent => compose(
   getContext({
     events: PropTypes.object
   }),
-  connect(null, { getNext }),
   mapProps(({ events: { onNext }, ...props }) => ({
     ...props,
-    onClick: combineHandlers([onNext, props.getNext, props.onClick]),
+    onClick: combineHandlers([onNext, props.onClick]),
   }))
 )((props) => <OriginalComponent {...props} />);
 
--- src/components/PreviousButtonEnhancer.js
+++ src/components/PreviousButtonEnhancer.js
@@ -3,18 +3,15 @@ import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import mapProps from 'recompose/mapProps';
 import getContext from 'recompose/getContext';
-import { getPrevious } from '../actions';
 import { combineHandlers } from '../utils/compositionUtils';
-import { connect } from '../utils/griddleConnect';
 
 const enhance = OriginalComponent => compose(
   getContext({
     events: PropTypes.object
   }),
-  connect(null, { getPrevious }),
   mapProps(({ events: { onPrevious }, ...props }) => ({
     ...props,
-    onClick: combineHandlers([onPrevious, props.getPrevious, props.onClick]),
+    onClick: combineHandlers([onPrevious, props.onClick]),
   }))
 )((props) => <OriginalComponent {...props} />);
 
